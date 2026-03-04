493b6757c0624aa525ccd9c9f7aa29bfb15af3a8
Fernando <dario.fernando.poveda@gmail.com>
2026-03-04 13:37:13 -0500

    Ejecucion

--- Migracion.Sistran/Engine/obj/Debug/net9.0/Engine.AssemblyInfo.cs
+++ Migracion.Sistran/Engine/obj/Debug/net9.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b4f5c8ae223c17d47fab230d212c55fd6854b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
